0c9a410f2e6f1bd7b2599dd7acf805c7e4b4b782
salamhusain <mshusain.cse@gmail.com>
2026-02-03 16:01:05 +0530

    Update models.py

--- editor/models.py
+++ editor/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class TemplateCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Template Categories"
+
+
 class Template(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='templates/')
+    category = models.ForeignKey(TemplateCategory, on_delete=models.SET_NULL, null=True, blank=True, related_name='templates')
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     is_admin_template = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -18,9 +32,9 @@ class Template(models.Model):
 
 class UserDesign(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True)
+    template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, blank=True)
     design_name = models.CharField(max_length=200)
-    canvas_data = models.JSONField()  # Stores canvas state
+    canvas_data = models.JSONField()
     preview_image = models.ImageField(upload_to='saved_designs/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
